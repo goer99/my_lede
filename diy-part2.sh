228a5ee0ca7620b91631f79a61e44f7b7f4f9dc6
goer99 <goer99@users.noreply.github.com>
2020-07-25 13:37:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,12 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# 将build日期写入GRUB TITLE
+cat >> .config <<EOF
+#CONFIG_KERNEL_BUILD_DOMAIN="Tom on $(date)"
+#CONFIG_KERNEL_BUILD_USER="Tom on $(date)"
+CONFIG_GRUB_TITLE="Lean OpenWrt AutoBuild by Tom on $(date)"
+EOF
 
 # 更改默认ip
 # sed -i 's/192.168.1.1/新的ip/g' package/base-files/files/bin/config_generate
