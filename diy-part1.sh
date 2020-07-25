3a5040c6274f041d119b5c15c3c374cdb62c18d0
goer99 <goer99@users.noreply.github.com>
2020-07-25 08:01:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# See /LICENSE for more information. 
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
