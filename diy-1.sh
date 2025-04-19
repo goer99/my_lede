703657ee072a9308fb27b45a2a818e29906e8773
xgit <goer99@users.noreply.github.com>
2025-04-19 09:51:51 +0800

    Update diy-1.sh

--- diy-1.sh
+++ diy-1.sh
@@ -9,3 +9,5 @@ sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.con
 # 添加 PassWall 插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+./scripts/feeds update -a && ./scripts/feeds install -a
